2ed0d7be74564383695c5485b820c467c0d1ffa7
jby9725 <jby9725@gmail.com>
2024-07-02 12:54:06 +0900

    p12950 행렬의 덧셈

---
+++ src/test/java/com/ki/practice/p12950/Tests.java
@@ -0,0 +1,21 @@
+package com.ki.practice.p12950;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+// 입출력 예
+// arr1	            arr2	        return
+// [[1,2],[2,3]]	[[3,4],[5,6]]	[[4,6],[7,9]]
+// [[1],[2]]	    [[3],[4]]	    [[4],[6]]
+
+public class Tests {
+    @Test
+    @DisplayName("[[1,2],[2,3]] , [[3,4],[5,6]] => [[4,6],[7,9]]")
+    void t1() {
+
+        assertThat(new Solution().solution(new int[][]{{1,2},{2,3}} , new int[][] {{3,4},{5,6}})).isEqualTo(new int[][]{{4,6},{7,9}});
+    }
+
+}
